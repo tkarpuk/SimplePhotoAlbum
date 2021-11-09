e386741f4364e8cb66c42fe6d7c7f115f821e39f
Taras Karpuk <taras.karpuk@gmail.com>
2021-11-09 17:53:56 +0200

    Small fix: initial album default

--- src/app/components/album/album.component.ts
+++ src/app/components/album/album.component.ts
@@ -8,7 +8,12 @@ import { Photo } from 'src/app/models/photo';
   styleUrls: ['./album.component.css']
 })
 export class AlbumComponent implements OnInit {
-  album!: Album;
+  album: Album = {
+    id: 1,
+    caption: 'First Album',
+    description: 'This album is first and only one in fake DB.'
+  };
+
   photos: Photo[] = [];
 
   constructor() { }
