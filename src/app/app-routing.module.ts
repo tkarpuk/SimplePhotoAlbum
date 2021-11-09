30c1445895d5baf577952aa670de2c3e78b39093
Taras Karpuk <taras.karpuk@gmail.com>
2021-11-09 10:39:12 +0200

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlbumComponent } from './components/album/album.component';
+import { LoginComponent } from './components/login/login.component';
+import { PhotoComponent } from './components/photo/photo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AlbumComponent },
+  { path: 'album', component: AlbumComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'photo/:id', component: PhotoComponent },
+  { path: '**', redirectTo: 'album' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
