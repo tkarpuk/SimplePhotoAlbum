44ab58dd60c6cbee6b49553de7aa5849c9fe7549
Taras Karpuk <taras.karpuk@gmail.com>
2021-11-09 10:02:15 +0200

    Add components: Album, Login, Photo, Photo-Edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './components/login/login.component';
+import { AlbumComponent } from './components/album/album.component';
+import { PhotoComponent } from './components/photo/photo.component';
+import { PhotoEditComponent } from './components/photo-edit/photo-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AlbumComponent,
+    PhotoComponent,
+    PhotoEditComponent
   ],
   imports: [
     BrowserModule,
