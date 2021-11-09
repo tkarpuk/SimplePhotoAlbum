71d44b66dc719d5ca79c430e763be1529bfbcd78
Taras Karpuk <taras.karpuk@gmail.com>
2021-11-09 16:14:23 +0200

    Photo-Edit simple implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AlbumComponent } from './components/album/album.component';
 import { LoginComponent } from './components/login/login.component';
-import { PhotoComponent } from './components/photo/photo.component';
+import { PhotoEditComponent } from './components/photo-edit/photo-edit.component';
 
 const routes: Routes = [
   { path: '', component: AlbumComponent },
   { path: 'album', component: AlbumComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'photo/:id', component: PhotoComponent },
+  { path: 'photo/:id', component: PhotoEditComponent },
   { path: '**', redirectTo: 'album' }
 ];
 
--- src/app/components/photo-edit/photo-edit.component.ts
+++ src/app/components/photo-edit/photo-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Photo } from 'src/app/models/photo';
 
 @Component({
   selector: 'app-photo-edit',
@@ -6,10 +9,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./photo-edit.component.css']
 })
 export class PhotoEditComponent implements OnInit {
+  formPhoto!: FormGroup;
+  id: number = 0;
+  photo: Photo = {
+    id: 0,
+    caption: '',
+    description: ''
+  };
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  )
+  { }
 
   ngOnInit(): void {
+
+    this.extractIdFromRoute();
+    if (this.id <= 0) {
+      // TODO: load data
+    }
+    this.createForm(this.id <= 0);
   }
 
+  extractIdFromRoute() {
+    this.id = this.activatedRoute.snapshot.params['id'];
+  }
+
+  createForm(needFileValidator: boolean = false) {
+    this.formPhoto = new FormGroup({
+      caption: new FormControl(this.photo.caption, [Validators.required]),
+      description: new FormControl(this.photo.description, [Validators.required]),
+      image: new FormControl(null)
+    });
+
+    if (needFileValidator)
+      this.formPhoto.get('image')?.addValidators([Validators.required]);
+  }
+
+  submitPhoto() {
+
+  }
+
+  cancelPhoto() {
+    this.router.navigateByUrl('/album');
+  }
 }
